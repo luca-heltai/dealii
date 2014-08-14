62ba2674790ed914b79cd2de211722fb59da559c
Luca Heltai <luca.heltai@sissa.it>
2014-08-14 14:37:28 +0200

    Added axis boundary 1.

---
+++ tests/opencascade/axis_boundary_01.cc
@@ -0,0 +1,71 @@
+
+//----------------------------  iges_read.cc  ---------------------------
+//    $Id: testsuite.html 13373 2006-07-13 13:12:08Z kanschat $
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005 by the deal.II authors 
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  iges_read.cc  ---------------------------
+
+
+// Read goteborg.iges and dump its topological structure to the logfile.
+
+#include "../tests.h"
+#include <fstream>
+#include <base/logstream.h>
+
+#include <deal.II/grid/occ_utilities.h>
+#include <deal.II/grid/occ_boundary_lib.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/grid_out.h>
+#include <deal.II/grid/grid_generator.h>
+
+#include <TopTools.hxx>
+#include <TopoDS_Shape.hxx>
+#include <TopoDS_Edge.hxx>
+#include <TopoDS_Face.hxx>
+#include <BRepFill.hxx>
+#include <Standard_Stream.hxx>
+
+using namespace OpenCASCADE;
+
+int main () 
+{
+  std::ofstream logfile("output");
+  // deallog.attach(logfile);
+  // deallog.depth_console(0);
+  
+  // Create a bspline passign through the points
+  std::vector<Point<3> > pts;
+  pts.push_back(Point<3>(0,0,0));
+  pts.push_back(Point<3>(0,1,0));
+  pts.push_back(Point<3>(1,1,0));
+  pts.push_back(Point<3>(1,0,0));
+
+  TopoDS_Edge edge1 = interpolation_curve(pts);
+  for(unsigned int i=0; i<pts.size(); ++i)
+    pts[i] += Point<3>(0,0,1);
+  TopoDS_Edge edge2 = interpolation_curve(pts);
+
+  TopoDS_Face face = BRepFill::Face (edge1, edge2);
+
+
+  AxisProjectionBoundary<1,3> manifold(face, Point<3>(0,1,0));
+  
+  Triangulation<1,3> tria;
+  GridGenerator::hyper_cube(tria);
+
+  tria.begin_active()->set_all_manifold_ids(1);
+  tria.set_manifold(1, manifold);
+  
+  tria.refine_global(4);
+  GridOut gridout;
+  gridout.write_msh(tria, logfile);
+
+  return 0;
+}                  
