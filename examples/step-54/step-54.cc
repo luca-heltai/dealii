e7fa7fa40a9dd6ef63005ce5dd668ce58a821a90
Andrea Mola <mola.andrea@gmail.com>
2014-10-21 16:59:20 +0200

    comments added to step-54

--- examples/step-54/step-54.cc
+++ examples/step-54/step-54.cc
@@ -19,22 +19,9 @@
 
 // @sect3{Include files}
 
-// The program starts with including a bunch of include files that we will use
+// The program starts with including a bunch of files that we will use
 // in the various parts of the program. Most of them have been discussed in
 // previous tutorials already:
-#include <deal.II/base/smartpointer.h>
-#include <deal.II/base/convergence_table.h>
-#include <deal.II/base/quadrature_lib.h>
-#include <deal.II/base/quadrature_selector.h>
-#include <deal.II/base/parsed_function.h>
-#include <deal.II/base/utilities.h>
-
-#include <deal.II/lac/full_matrix.h>
-#include <deal.II/lac/vector.h>
-#include <deal.II/lac/solver_control.h>
-#include <deal.II/lac/solver_gmres.h>
-#include <deal.II/lac/precondition.h>
-
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/tria_iterator.h>
 #include <deal.II/grid/tria_accessor.h>
@@ -42,21 +29,16 @@
 #include <deal.II/grid/grid_in.h>
 #include <deal.II/grid/grid_out.h>
 #include <deal.II/grid/tria_boundary_lib.h>
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/vector_tools.h>
 
 // And here are the headers of the opencascade support classes and functions:
 #include <deal.II/opencascade/boundary_lib.h>
 #include <deal.II/opencascade/utilities.h>
 
-#include <deal.II/dofs/dof_handler.h>
-#include <deal.II/dofs/dof_accessor.h>
-#include <deal.II/dofs/dof_tools.h>
 
-#include <deal.II/fe/fe_q.h>
-#include <deal.II/fe/fe_values.h>
-#include <deal.II/fe/mapping_q.h>
 
-#include <deal.II/numerics/data_out.h>
-#include <deal.II/numerics/vector_tools.h>
+
 
 // And here are a few C++ standard header files that we will need:
 #include <cmath>
