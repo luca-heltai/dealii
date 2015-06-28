92eff78fc012137c878436053ee87be13935f947
Luca Heltai <luca.heltai@sissa.it>
2015-06-29 00:34:15 +0200

    Added trilinos.

--- contrib/utilities/setup_travis.sh
+++ contrib/utilities/setup_travis.sh
@@ -16,4 +16,20 @@ else
   sudo apt-get update -qq
   sudo apt-get install cmake
   sudo apt-get install ninja-build
+
+  # Trilinos
+  sudo add-apt-repository -y ppa:nschloe/trilinos-nightly
+  sudo add-apt-repository -y ppa:nschloe/netcdf-nightly
+  sudo apt-get update -qq
+  sudo apt-get install libopenmpi-dev openmpi-bin
+  # The -dev versions are definitely not needed since Trilinos doesn't expose
+  # any of the interface of libptscotch. This can be fixed by the PRIVATE
+  # keyword in the appropriate target_link_libraries() in Trilinos.
+  sudo apt-get install libptscotch-dev libmumps-dev libboost-program-options-dev
+  sudo apt-get install libboost-system-dev liblapack-dev binutils-dev
+  sudo apt-get install libhdf5-openmpi-7 libhdf5-openmpi-dev
+  sudo apt-get install libnetcdf-dev
+  sudo apt-get install libboost-dev libtbb-dev # should be added by Trilinos
+  sudo apt-get install cmake-data cmake
+  sudo apt-get install trilinos-dev
 fi
