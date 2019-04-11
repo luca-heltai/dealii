781b377a25eadcc02de2cbecf48c0c1fd4e70b34
Luca Heltai <luca.heltai@sissa.it>
2019-04-11 15:53:05 +0200

    Added ConvolutionCoupling (working version)

--- include/deal.II/non_matching/coupling.h
+++ include/deal.II/non_matching/coupling.h
@@ -24,6 +24,7 @@
 
 #include <deal.II/fe/component_mask.h>
 #include <deal.II/fe/fe.h>
+#include <deal.II/fe/fe_values.h>
 #include <deal.II/fe/mapping_q1.h>
 
 #include <deal.II/grid/grid_tools_cache.h>
@@ -221,6 +222,355 @@ namespace NonMatching
     const ComponentMask &          immersed_comps = ComponentMask(),
     const Mapping<dim1, spacedim> &immersed_mapping =
       StaticMappingQ1<dim1, spacedim>::mapping);
+
+  /**
+   * Create a coupling sparsity pattern for non-matching independent grids,
+   * using a convolution kernel with compact support of radius epsilon.
+   *
+   * Given two non-matching triangulations, representing the domains $\Omega^0$
+   * and $\Omega^1$, both embedded in $R^d$, and two finite element spaces
+   * $V^0(\Omega^0) = \text{span}\{v_i\}_{i=0}^n$ and $V^1(\Omega^1) =
+   * \text{span}\{w_\alpha\}_{\alpha=0}^m$, compute the sparsity pattern that
+   * would be necessary to assemble the matrix
+   *
+   * \f[
+   * M_{i\alpha} \dealcoloneq \int_{\Omega^0} \int_{\Omega^1}
+   * v_i(x) \delta^{\epsilon}(x-y) w_\alpha(y) dx \ dy,
+   * \quad i \in [0,n), \alpha \in [0,m),
+   * \f]
+   *
+   * where $V^0(\Omega^0)$ is the finite element space associated with the
+   * `dh0` passed to this function (or part of it, if specified in
+   * `comps0`), while $V^1(\Omega^1)$ is the finite element space associated
+   * with the `dh1` passed to this function (or part of it, if specified
+   * in `comps1`).
+   *
+   * The `comps0` and `comps1` masks are assumed to be ordered in
+   * the same way: the first component of `comps0` will couple with the
+   * first component of `comps1`, the second with the second, and so
+   * on. If one of the two masks has more non-zero than the other, then the
+   * excess components will be ignored.
+   *
+   * For both spaces, it is possible to specify a custom Mapping, which
+   * defaults to StaticMappingQ1 for both.
+   *
+   * This function will also work in parallel, provided that the second
+   * triangulation is of type parallel::shared::Triangulation<dim1,spacedim>.
+   * An exception is thrown if you use a slave
+   * parallel::distributed::Triangulation<dim1,spacedim>.
+   *
+   * This method assumes that you make a convolution with a function that has
+   * support in `[-epsilon, epsilon]`.
+   *
+   * @author Luca Heltai, 2019.
+   */
+  template <int dim0,
+            int dim1,
+            int spacedim,
+            typename Sparsity,
+            typename Number>
+  void
+  create_coupling_sparsity_pattern(
+    const Number &                          epsilon,
+    const GridTools::Cache<dim0, spacedim> &cache0,
+    const GridTools::Cache<dim1, spacedim> &cache1,
+    const DoFHandler<dim0, spacedim> &      dh0,
+    const DoFHandler<dim1, spacedim> &      dh1,
+    Sparsity &                              sparsity,
+    const AffineConstraints<Number> &constraints = AffineConstraints<Number>(),
+    const ComponentMask &            comps0      = ComponentMask(),
+    const ComponentMask &            comps1      = ComponentMask())
+  {
+    if (epsilon == 0.0)
+      {
+        create_coupling_sparsity_pattern(cache0,
+                                         dh0,
+                                         dh1,
+                                         QGauss<dim1>(2 * dh1.get_fe().degree +
+                                                      1),
+                                         sparsity,
+                                         constraints,
+                                         comps0,
+                                         comps1,
+                                         cache1.get_mapping());
+        return;
+      }
+    AssertDimension(sparsity.n_rows(), dh0.n_dofs());
+    AssertDimension(sparsity.n_cols(), dh1.n_dofs());
+    Assert((dynamic_cast<
+              const parallel::distributed::Triangulation<dim1, spacedim> *>(
+              &dh1.get_triangulation()) == nullptr),
+           ExcNotImplemented());
+
+    const auto &fe0 = dh0.get_fe();
+    const auto &fe1 = dh1.get_fe();
+
+    // Dof indices
+    std::vector<types::global_dof_index> dofs0(fe0.dofs_per_cell);
+    std::vector<types::global_dof_index> dofs1(fe1.dofs_per_cell);
+
+    // Take care of components
+    const ComponentMask mask0 =
+      (comps0.size() == 0 ? ComponentMask(fe0.n_components(), true) : comps0);
+
+    const ComponentMask mask1 =
+      (comps1.size() == 0 ? ComponentMask(fe1.n_components(), true) : comps1);
+
+    AssertDimension(mask0.size(), fe0.n_components());
+    AssertDimension(mask1.size(), fe1.n_components());
+
+    // Global to local indices
+    std::vector<unsigned int> gtl0(fe0.n_components(),
+                                   numbers::invalid_unsigned_int);
+    std::vector<unsigned int> gtl1(fe1.n_components(),
+                                   numbers::invalid_unsigned_int);
+
+    for (unsigned int i = 0, j = 0; i < gtl0.size(); ++i)
+      if (mask0[i])
+        gtl0[i] = j++;
+
+    for (unsigned int i = 0, j = 0; i < gtl1.size(); ++i)
+      if (mask1[i])
+        gtl1[i] = j++;
+
+    const auto &tree1 = cache1.get_cell_bounding_boxes_rtree();
+
+    std::vector<
+      std::pair<BoundingBox<spacedim>,
+                typename Triangulation<dim1, spacedim>::active_cell_iterator>>
+      intersection;
+
+    for (const auto &cell0 : dh0.active_cell_iterators())
+      if (cell0->is_locally_owned())
+        {
+          intersection.resize(0);
+          auto box0 = cell0->bounding_box();
+          box0.extend(epsilon);
+          boost::geometry::index::query(tree1,
+                                        boost::geometry::index::intersects(
+                                          box0),
+                                        std::back_inserter(intersection));
+          if (!intersection.empty())
+            {
+              cell0->get_dof_indices(dofs0);
+              for (const auto &entry : intersection)
+                {
+                  typename DoFHandler<dim1, spacedim>::cell_iterator cell1(
+                    *entry.second, &dh1);
+                  cell1->get_dof_indices(dofs1);
+                  constraints.add_entries_local_to_global(dofs0,
+                                                          dofs1,
+                                                          sparsity);
+                }
+            }
+        }
+  }
+
+
+
+  /**
+   * Create a coupling  mass matrix for non-matching independent grids,
+   * using a convolution kernel with compact support.
+   *
+   * Given two non-matching triangulations, representing the domains
+   * $\Omega^0$ and $\Omega^1$, both embedded in $R^d$, and two finite element
+   * spaces $V^0(\Omega^0) = \text{span}\{v_i\}_{i=0}^n$ and $V^1(\Omega^1) =
+   * \text{span}\{w_\alpha\}_{\alpha=0}^m$, compute the matrix
+   *
+   * \f[
+   * M_{i\alpha} \dealcoloneq \int_{\Omega^0} \int_{\Omega^1}
+   * v_i(x) \delta^{\epsilon}(x-y) w_\alpha(y) dx \ dy,
+   * \quad i \in [0,n), \alpha \in [0,m),
+   * \f]
+   *
+   * where $V^0(\Omega^0)$ is the finite element space associated with the
+   * `dh0` passed to this function (or part of it, if specified in
+   * `comps0`), while $V^1(\Omega^1)$ is the finite element space associated
+   * with the `dh1` passed to this function (or part of it, if specified
+   * in `comps1`).
+   *
+   * The corresponding sparsity patterns can be computed by calling the
+   * make_coupling_sparsity_pattern() function.
+   *
+   * The `comps0` and `comps1` masks are assumed to be ordered in
+   * the same way: the first component of `comps0` will couple with the
+   * first component of `comps1`, the second with the second, and so
+   * on. If one of the two masks has more non-zero than the other, then the
+   * excess components will be ignored.
+   *
+   * For both spaces, it is possible to specify a custom Mapping, which
+   * defaults to StaticMappingQ1 for both.
+   *
+   * This function will also work in parallel, provided that the second
+   * triangulation is of type parallel::shared::Triangulation<dim1,spacedim>.
+   * An exception is thrown if you use a slave
+   * parallel::distributed::Triangulation<dim1,spacedim>.
+   *
+   * The one dimensional function @p kernel you pass to this method must have
+   * compact support equal to $[-1,1]$. The convolution is made with the
+   * function
+   *
+   * \f[
+   * \delta^\epsilon(r) := \frac{1}{\epsilon^d} \prod_{i=1}^{d}
+   *  K\left(\frac{r_i}{\epsilon}\right),
+   * \f]
+   *
+   * where $K$ is the function @p kernel, and $\epsilon$ is the parametr
+   * @p epsilon, representing the size of the support used to compute the
+   * convolution.
+   *
+   * @author Luca Heltai, 2019.
+   */
+  template <int dim0, int dim1, int spacedim, typename Matrix, typename Number>
+  void
+  create_coupling_mass_matrix(
+    const Function<1> &                     kernel,
+    const Number &                          epsilon,
+    const GridTools::Cache<dim0, spacedim> &cache0,
+    const GridTools::Cache<dim1, spacedim> &cache1,
+    const DoFHandler<dim0, spacedim> &      dh0,
+    const DoFHandler<dim1, spacedim> &      dh1,
+    const Quadrature<dim0> &                quadrature0,
+    const Quadrature<dim1> &                quadrature1,
+    Matrix &                                matrix,
+    const AffineConstraints<Number> &constraints = AffineConstraints<Number>(),
+    const ComponentMask &            comps0      = ComponentMask(),
+    const ComponentMask &            comps1      = ComponentMask())
+  {
+    if (epsilon == 0)
+      {
+        create_coupling_mass_matrix(cache0,
+                                    dh0,
+                                    dh1,
+                                    quadrature1,
+                                    matrix,
+                                    constraints,
+                                    comps0,
+                                    comps1,
+                                    cache1.get_mapping());
+        return;
+      }
+
+    AssertDimension(matrix.m(), dh0.n_dofs());
+    AssertDimension(matrix.n(), dh1.n_dofs());
+    Assert((dynamic_cast<
+              const parallel::distributed::Triangulation<dim1, spacedim> *>(
+              &dh1.get_triangulation()) == nullptr),
+           ExcNotImplemented());
+
+    const auto &fe0 = dh0.get_fe();
+    const auto &fe1 = dh1.get_fe();
+
+    FEValues<dim0, spacedim> fev0(cache0.get_mapping(),
+                                  fe0,
+                                  quadrature0,
+                                  update_values | update_JxW_values |
+                                    update_quadrature_points);
+
+    FEValues<dim1, spacedim> fev1(cache1.get_mapping(),
+                                  fe1,
+                                  quadrature1,
+                                  update_values | update_JxW_values |
+                                    update_quadrature_points);
+
+    // Dof indices
+    std::vector<types::global_dof_index> dofs0(fe0.dofs_per_cell);
+    std::vector<types::global_dof_index> dofs1(fe1.dofs_per_cell);
+
+    // Local Matrix
+    FullMatrix<double> cell_matrix(fe0.dofs_per_cell, fe1.dofs_per_cell);
+
+    // Take care of components
+    const ComponentMask mask0 =
+      (comps0.size() == 0 ? ComponentMask(fe0.n_components(), true) : comps0);
+
+    const ComponentMask mask1 =
+      (comps1.size() == 0 ? ComponentMask(fe1.n_components(), true) : comps1);
+
+    AssertDimension(mask0.size(), fe0.n_components());
+    AssertDimension(mask1.size(), fe1.n_components());
+
+    // Global to local indices
+    std::vector<unsigned int> gtl0(fe0.n_components(),
+                                   numbers::invalid_unsigned_int);
+    std::vector<unsigned int> gtl1(fe1.n_components(),
+                                   numbers::invalid_unsigned_int);
+
+    for (unsigned int i = 0, j = 0; i < gtl0.size(); ++i)
+      if (mask0[i])
+        gtl0[i] = j++;
+
+    for (unsigned int i = 0, j = 0; i < gtl1.size(); ++i)
+      if (mask1[i])
+        gtl1[i] = j++;
+
+    const auto &tree1 = cache1.get_cell_bounding_boxes_rtree();
+
+    std::vector<
+      std::pair<BoundingBox<spacedim>,
+                typename Triangulation<dim1, spacedim>::active_cell_iterator>>
+      intersection;
+
+    for (const auto &cell0 : dh0.active_cell_iterators())
+      if (cell0->is_locally_owned())
+        {
+          intersection.resize(0);
+          auto box0 = cell0->bounding_box();
+          box0.extend(epsilon);
+          boost::geometry::index::query(tree1,
+                                        boost::geometry::index::intersects(
+                                          box0),
+                                        std::back_inserter(intersection));
+          if (!intersection.empty())
+            {
+              cell0->get_dof_indices(dofs0);
+              fev0.reinit(cell0);
+              for (const auto &entry : intersection)
+                {
+                  typename DoFHandler<dim1, spacedim>::cell_iterator cell1(
+                    *entry.second, &dh1);
+                  cell1->get_dof_indices(dofs1);
+                  fev1.reinit(cell1);
+                  cell_matrix = 0;
+                  for (unsigned int q0 = 0; q0 < quadrature0.size(); ++q0)
+                    for (unsigned int q1 = 0; q1 < quadrature1.size(); ++q1)
+                      {
+                        double delta_q0q1 = 1;
+                        for (unsigned int d = 0; d < spacedim; ++d)
+                          delta_q0q1 *= kernel.value(Point<1>(
+                                          (fev0.quadrature_point(q0)[d] -
+                                           fev1.quadrature_point(q1)[d]) /
+                                          epsilon)) /
+                                        epsilon;
+                        for (unsigned int i = 0; i < fe0.dofs_per_cell; ++i)
+                          {
+                            const auto comp_i =
+                              fe0.system_to_component_index(i).first;
+                            if (mask0[comp_i])
+                              for (unsigned int j = 0; j < fe1.dofs_per_cell;
+                                   ++j)
+                                {
+                                  const auto comp_j =
+                                    fe1.system_to_component_index(j).first;
+                                  if (gtl1[comp_j] == gtl0[comp_i])
+                                    {
+                                      cell_matrix(i, j) +=
+                                        fev0.shape_value(i, q0) *
+                                        fev1.shape_value(j, q1) * delta_q0q1 *
+                                        fev0.JxW(q0) * fev1.JxW(q1);
+                                    }
+                                }
+                          }
+                      }
+
+                  constraints.distribute_local_to_global(cell_matrix,
+                                                         dofs0,
+                                                         dofs1,
+                                                         matrix);
+                }
+            }
+        }
+  }
 } // namespace NonMatching
 DEAL_II_NAMESPACE_CLOSE
 
