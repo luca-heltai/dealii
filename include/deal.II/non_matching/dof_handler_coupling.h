1e0d20001684dd537996c2f0811fd2b12e4579c6
Luca Heltai <luca.heltai@sissa.it>
2019-11-20 09:23:00 +0100

    RM ME

--- include/deal.II/non_matching/dof_handler_coupling.h
+++ include/deal.II/non_matching/dof_handler_coupling.h
@@ -18,6 +18,7 @@
 
 #include <deal.II/base/config.h>
 
+#include <deal.II/base/iterator_range.h>
 #include <deal.II/base/quadrature.h>
 
 #include <deal.II/dofs/dof_handler.h>
@@ -31,6 +32,8 @@
 #include <deal.II/grid/grid_tools_cache.h>
 
 #include <deal.II/lac/affine_constraints.h>
+
+#include <deal.II/non_matching/immersed_surface_quadrature.h>
 
 #include <deal.II/particles/generators.h>
 #include <deal.II/particles/particle_handler.h>
@@ -112,15 +115,32 @@ namespace NonMatching
           communicator = tr->get_communicator();
           mpi_proc     = Utilities::MPI::this_mpi_process(communicator);
           n_mpi_procs  = Utilities::MPI::n_mpi_processes(communicator);
+
+
+          // Distribute the local points to the processor that owns
+          // them on the triangulation
+          auto my_bounding_box = GridTools::compute_mesh_predicate_bounding_box(
+            *tria1, IteratorFilters::LocallyOwnedCell());
+
+          global_bounding_boxes =
+            Utilities::MPI::all_gather(communicator, my_bounding_box);
         }
       else
         {
           communicator = MPI_COMM_WORLD;
           mpi_proc     = 0;
           n_mpi_procs  = 1;
+
+          global_bounding_boxes = {
+            GridTools::compute_mesh_predicate_bounding_box(
+              *tria1, IteratorFilters::LocallyOwnedCell())};
         }
     };
 
+
+    using CellsAndQuadratureIterator = typename std::vector<
+      std::pair<typename DoFHandler<spacedim>::iterator,
+                ImmersedSurfaceQuadrature<spacedim>>>::iterator;
 
     /**
      * @brief DoFHandlerCoupling
@@ -217,6 +237,77 @@ namespace NonMatching
         }
     }
 
+    /**
+     * Assemble sparsity for Nitsche method.
+     *
+     * @param[in] quadrature
+     * @param[out] sparsity
+     * @param[in] constraints
+     * @param[in] reuse_internal_data_structures
+     */
+    template <class Matrix, class Vector, typename number = double>
+    void
+    create_nitsche_restriction(
+      const Quadrature<dim> &          quadrature,
+      const Function<spacedim> &       rhs_function,
+      Matrix &                         matrix,
+      Vector &                         rhs,
+      const AffineConstraints<number> &constraints =
+        AffineConstraints<number>(),
+      const bool reuse_internal_data_structures = false) const
+    {
+      possibly_generate_particle_handler(reuse_internal_data_structures,
+                                         quadrature);
+
+      std::vector<types::global_dof_index> dof_indices1(fe1->dofs_per_cell);
+
+
+      FullMatrix<double> local_matrix(fe1->dofs_per_cell, fe1->dofs_per_cell);
+      Vector<double>     local_rhs(fe1->dofs_per_cell);
+
+      auto particle = quadrature_particle_handler->begin();
+      while (particle != particle_handler->end())
+        {
+          local_matrix     = 0;
+          local_rhs        = 0;
+          const auto &cell = particle->get_surrounding_cell(*tria1);
+          const auto &dh_cell =
+            typename DoFHandler<dim, spacedim>::cell_iterator(*cell, dh1);
+          dh_cell->get_dof_indices(dof_indices1);
+
+          const auto pic = particle_handler->particles_in_cell(cell);
+
+          for (const auto &p : pic)
+            {
+              const auto  ref_q      = p->get_reference_location();
+              const auto  real_q     = p->get_location();
+              const auto  properties = p->get_properties();
+              const auto &JxW        = properties[0];
+              for (unsigned int i = 0; i < fe1->dofs_per_cell; ++i)
+                {
+                  const auto comp_i = fe1->system_to_component_index(i).first;
+                  if (comps1[comp_i])
+                    {
+                      for (unsigned int j = 0; j < fe1->dofs_per_cell; ++j)
+                        {
+                          const auto comp_j =
+                            fe1->system_to_component_index(j).first;
+                          if (comp_i == comp_j)
+                            local_matrix(i, j) += fe1->shape_value(i, ref_q) *
+                                                  fe1->shape_value(j, ref_q) *
+                                                  JxW;
+                        }
+                      local_rhs(i) += rhs_function.value(real_q) *
+                                      fe1->shape_value(i, ref_q) * JxW;
+                    }
+                }
+            }
+          constraints.distribute_local_to_global(
+            local_matrix, local_rhs, dof_indices, matrix, rhs);
+        }
+    }
+
+
     template <class Matrix, typename number = double>
     void
     create_interpolation_matrix(Matrix &                         matrix,
@@ -297,80 +388,154 @@ namespace NonMatching
       return *particle_handler;
     }
 
+    const Particles::ParticleHandler<spacedim> &
+    get_quadrature_particle_handler() const
+    {
+      Assert(quadrature_particle_handler,
+             ExcMessage("You need to call possibly_generate_particle_handler "
+                        "function first."));
+      return *quadrature_particle_handler;
+    }
+
     void
     possibly_generate_particle_handler(
-      bool reuse_internal_data_structures) const
+      bool                   reuse_internal_data_structures,
+      const Quadrature<dim> &quadrature = Quadrature<dim>()) const
     {
-      if (reuse_internal_data_structures == false || !particle_handler)
+      if (quadrature.size() == 0)
         {
-          auto tr = dynamic_cast<
-            const parallel::distributed::Triangulation<spacedim> *>(&(*tria1));
-
-          particle_handler =
-            std::make_unique<Particles::ParticleHandler<dim, spacedim>>(
-              *tr, *mapping1, n_comps + 1);
-
-          ComponentMask temp_mask(fe2->n_components(), false);
-          unsigned int  first_comp = 0;
-          for (; first_comp < fe2->n_components(); ++first_comp)
-            if (comps2[first_comp])
-              {
-                temp_mask.set(first_comp, true);
-                break;
-              }
-
-          std::map<types::global_dof_index, Point<spacedim>> support_points_map;
-          DoFTools::map_dofs_to_support_points(*mapping2,
-                                               *dh2,
-                                               support_points_map,
-                                               temp_mask);
-
-          const auto dh2_selected_indices =
-            DoFTools::extract_dofs_per_component(*dh2,
-                                                 DoFTools::OwnershipType::owned,
-                                                 comps2);
-
-          std::vector<Point<spacedim>> support_points_vec;
-          support_points_vec.reserve(support_points_map.size());
-
-
-          for (auto const &element : support_points_map)
-            if (dh2_selected_indices[first_comp].is_element(element.first))
-              support_points_vec.emplace_back(element.second);
-
-          std::vector<double> properties(support_points_vec.size() *
-                                         (n_comps + 1));
-
-          std::vector<typename IndexSet::ElementIterator> elements;
-
-          unsigned int j = 0;
-          for (unsigned int c = first_comp; c < comps2.size() && j < n_comps;
-               ++c)
-            if (comps2[c])
-              elements.emplace_back(dh2_selected_indices[c].begin());
-
-          for (unsigned int i = 0; i < support_points_vec.size(); ++i)
+          if (reuse_internal_data_structures == false || !particle_handler)
             {
-              properties[i * (n_comps + 1)] = mpi_proc;
-              for (unsigned int c = 0; c < n_comps; ++c)
-                properties[i * (n_comps + 1) + c + 1] = *(elements[c]++);
+              auto tr = dynamic_cast<
+                const parallel::distributed::Triangulation<spacedim> *>(
+                &(*tria1));
+
+              particle_handler =
+                std::make_unique<Particles::ParticleHandler<dim, spacedim>>(
+                  *tr, *mapping1, n_comps + 1);
+
+              ComponentMask temp_mask(fe2->n_components(), false);
+              unsigned int  first_comp = 0;
+              for (; first_comp < fe2->n_components(); ++first_comp)
+                if (comps2[first_comp])
+                  {
+                    temp_mask.set(first_comp, true);
+                    break;
+                  }
+
+              std::map<types::global_dof_index, Point<spacedim>>
+                support_points_map;
+              DoFTools::map_dofs_to_support_points(*mapping2,
+                                                   *dh2,
+                                                   support_points_map,
+                                                   temp_mask);
+
+              const auto dh2_selected_indices =
+                DoFTools::extract_dofs_per_component(
+                  *dh2, DoFTools::OwnershipType::owned, comps2);
+
+              std::vector<Point<spacedim>> support_points_vec;
+              support_points_vec.reserve(support_points_map.size());
+
+
+              for (auto const &element : support_points_map)
+                if (dh2_selected_indices[first_comp].is_element(element.first))
+                  support_points_vec.emplace_back(element.second);
+
+              std::vector<double> properties(support_points_vec.size() *
+                                             (n_comps + 1));
+
+              std::vector<typename IndexSet::ElementIterator> elements;
+
+              unsigned int j = 0;
+              for (unsigned int c = first_comp;
+                   c < comps2.size() && j < n_comps;
+                   ++c)
+                if (comps2[c])
+                  elements.emplace_back(dh2_selected_indices[c].begin());
+
+              for (unsigned int i = 0; i < support_points_vec.size(); ++i)
+                {
+                  properties[i * (n_comps + 1)] = mpi_proc;
+                  for (unsigned int c = 0; c < n_comps; ++c)
+                    properties[i * (n_comps + 1) + c + 1] = *(elements[c]++);
+                }
+
+              auto cpu_to_index =
+                particle_handler->insert_global_particles(support_points_vec,
+                                                          global_bounding_boxes,
+                                                          properties);
             }
+        }
+      else
+        {
+          if (reuse_internal_data_structures == false ||
+              !quadrature_particle_handler)
+            {
+              auto tr1 = dynamic_cast<
+                const parallel::distributed::Triangulation<spacedim> *>(
+                &(*tria1));
+              Assert(tr1,
+                     ExcMessage("Only available for parallel distributed "
+                                "triangulations"));
+
+              auto tr2 = dynamic_cast<
+                const parallel::distributed::Triangulation<spacedim> *>(
+                &(*tria2));
+
+              Assert(tr2,
+                     ExcMessage("Only available for parallel distributed "
+                                "triangulations"));
+
+              const unsigned int n_properties =
+                (dim == spacedim) ? 1 : spacedim + 1;
+
+              quadrature_particle_handler =
+                std::make_unique<Particles::ParticleHandler<dim, spacedim>>(
+                  *tr1, *mapping1, n_properties);
 
 
-          // Distribute the local points to the processor that owns them
-          // on the triangulation
-          auto my_bounding_box = GridTools::compute_mesh_predicate_bounding_box(
-            *tr, IteratorFilters::LocallyOwnedCell());
+              std::vector<Point<spacedim>> quadrature_points_vec(
+                quadrature.size() * tr2->n_locally_owned_active_cells());
 
-          auto global_bounding_boxes =
-            Utilities::MPI::all_gather(communicator, my_bounding_box);
+              std::vector<double> properties(
+                tr2->n_locally_owned_active_cells() * n_properties);
 
-          auto cpu_to_index =
-            particle_handler->insert_global_particles(support_points_vec,
-                                                      global_bounding_boxes,
-                                                      properties);
+              UpdateFlags flags = update_JxW_values | update_quadrature_points;
+              if (spacedim > dim)
+                flags |= update_normal_vectors;
+              FEValues<dim, spacedim> fe_v(*mapping2, *fe2, quadrature, flags);
+
+              unsigned int i = 0;
+              for (const auto &cell : dh2->active_cell_iterators())
+                if (cell->is_locally_owned())
+                  {
+                    fe_v.reinit(cell);
+                    const auto &points = fe_v.get_quadrature_points();
+                    const auto &JxW    = fe_v.get_JxW_values();
+
+                    for (unsigned int q = 0; q < points.size(); ++q)
+                      {
+                        quadrature_points_vec[i * points.size() + q] =
+                          points[q];
+                        properties[i * n_properties] = JxW[q];
+                        if (dim < spacedim)
+                          for (unsigned int d = 0; d < spacedim; ++d)
+                            {
+                              properties[i * n_properties + 1 + d] =
+                                fe_v.normal_vector(q)[d];
+                            }
+                      }
+                  }
+              auto cpu_to_index =
+                particle_handler->insert_global_particles(quadrature_points_vec,
+                                                          global_bounding_boxes,
+                                                          properties);
+            }
         }
     }
+
+
 
   private:
     SmartPointer<const Mapping<spacedim>>      mapping1;
@@ -391,6 +556,10 @@ namespace NonMatching
     SmartPointer<const GridTools::Cache<spacedim>>      cache1;
     SmartPointer<const GridTools::Cache<dim, spacedim>> cache2;
 
+    std::vector<std::pair<typename DoFHandler<spacedim>::iterator,
+                          ImmersedSurfaceQuadrature<spacedim>>>
+      cells_and_quadratures;
+
     ComponentMask comps1;
     ComponentMask comps2;
 
@@ -402,8 +571,13 @@ namespace NonMatching
     std::vector<unsigned int> gtl1;
     std::vector<unsigned int> gtl2;
 
+    std::vector<std::vector<BoundingBox<spacedim>>> global_bounding_boxes;
+
     mutable std::unique_ptr<Particles::ParticleHandler<spacedim>>
       particle_handler;
+
+    mutable std::unique_ptr<Particles::ParticleHandler<spacedim>>
+      quadrature_particle_handler;
   };
 
 } // namespace NonMatching
